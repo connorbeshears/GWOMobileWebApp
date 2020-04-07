a8a0c444db4d145ec83faae9e352d094956171e2
connorbeshears <connorbeshears@gmail.com>
2020-04-07 17:04:17 -0500

    scanner in, still needs work

--- scripts/cameraScanner.js
+++ scripts/cameraScanner.js
@@ -1,18 +1,20 @@
-import Quagga from '../dist/quagga.min.js'; // ES6
+
+console.log("Started Camera thing")
 
 Quagga.init({
-    inputStream : {
-      name : "Live",
-      type : "LiveStream"
-    },
-    decoder : {
-      readers : ["code_128_reader"]
+  inputStream : {
+    name : "Live",
+    type : "LiveStream",
+    target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
+  },
+  decoder : {
+    readers : ["code_128_reader"]
+  }
+}, function(err) {
+    if (err) {
+        console.log(err);
+        return
     }
-  }, function(err) {
-      if (err) {
-          console.log(err);
-          return
-      }
-      console.log("Initialization finished. Ready to start");
-      Quagga.start();
-  });
+    console.log("Initialization finished. Ready to start");
+    Quagga.start();
+});
